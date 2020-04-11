9cb6c2bea18294dcae07641cc32a8b390ae2cea3
Sugiri <masugiri@amd.com>
2020-04-11 13:58:33 +0800

    Completed Step 4

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from app import app #the flask application is app, a member of the app package
+from app import app, db #the flask application is app, a member of the app package
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
