32a62df662b9d40d924e592638a67537bac62f7c
Sugiri <masugiri@amd.com>
2020-04-11 16:33:16 +0800

    Fixed internal server error after registering

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
